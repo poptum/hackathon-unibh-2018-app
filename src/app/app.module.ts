3df6c162fe2cee0b87ac7010fc8cafa2df167a98
andre <poptum5@gmail.com>
2018-06-10 08:37:05 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,15 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { IonicStorageModule } from '@ionic/storage';
+import { Geolocation } from '@ionic-native/geolocation';
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+
 
 @NgModule({
   declarations: [
@@ -21,7 +28,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    SocketIoModule.forRoot(config),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,7 +43,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Geolocation
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Socket } from 'ng-socket-io';
+import { Observable } from 'rxjs/Observable';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  ocorrencia: boolean = false;
+  tipooc: any;
 
+  constructor(public navCtrl: NavController, private socket: Socket, private geolocation: Geolocation, private storage: Storage) {
+
+    this.getAgentes().subscribe(message => {
+      this.geolocation.getCurrentPosition().then((resp) => {
+        this.socket.emit('localizacao', { lat: resp.coords.latitude, lng: resp.coords.longitude, ocorrencia: this.tipooc});
+        // resp.coords.latitude
+        // resp.coords.longitude
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
+    });
+
+  }
+  ionViewDidLoad() {
+    this.socket.connect();
+
+    this.storage.get('tipoOcorrencia').then((val) => {
+      if (!val) {
+        this.ocorrencia = false
+      } else {
+        this.ocorrencia = true
+      }
+    });
+
+  }
+
+  getAgentes() {
+    let observable = new Observable(observer => {
+      this.socket.on('ocorrencia', (data) => {
+        observer.next(data);
+      });
+    })
+    return observable;
+  }
+
+  onChangeTipo() {
+    this.storage.set('tipoOcorrencia', this.tipooc);
+  }
+
+  ngOnDestroy() {
+    this.socket.disconnect();
   }
 
 }
